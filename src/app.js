1367718c5e96374544d94d2f99f254b5967069fd
Prima Aulia Gusta <primaulia@gmail.com>
2019-02-05 15:21:02 +0800

    initial commit for garate setup

---
+++ src/app.js
@@ -0,0 +1,23 @@
+// TODO: Build an awesome garage!
+
+// CRD
+// C = Create new car and put it inside the garage
+// R = Read all cars available in the garage
+// R = Read one car by id
+// D = Delete existing car by choosing car id
+
+// TODO: Build a modular garage app. Remember Single Responsibility Principle
+// 1 Function = 1 Responsibility
+
+// R
+// 1. Send an AJAX request to the API to fetch all cars
+// 2. For each car
+//    3. Insert a new row in the cars table
+
+
+// C
+// 1. Add an event listener on the form
+// 2. Prevent default behavior when we submit it
+// 3. Recover data entered by the user
+// 4. Send a POST ajax request to the API
+// 5. Refresh cars list
